5a661126c0db25bd9e33ff5803a4ba568d2f9533
Rafał Ileczko <rileczko@gmail.com>
2024-05-24 08:46:56 +0200

    [packages/vanilla] Return container metadata after fetch

--- packages/vanilla/src/utils.ts
+++ packages/vanilla/src/utils.ts
@@ -13,19 +13,20 @@ export function parseContainer(
 ): Record<string, any>;
 
 export function parseContainer(
-  container: { data: any } | any[] | null | undefined,
+  container: { data: any[] } | any[] | null | undefined,
   opts: any = {},
 ) {
   if (!container) return null;
 
   const { returnObject } = opts;
-  const data = ((container as any)?.data || container) as {
+  const isFullContainer = (container as any)?.data;
+  const rawData = ((container as any)?.data || container) as {
     name: string;
     value: unknown;
   }[];
 
   if (returnObject) {
-    return data.reduce((acc, { name, value }) => {
+    const data = rawData.reduce((acc, { name, value }) => {
       return {
         ...acc,
         [name]: isObject(value)
@@ -37,14 +38,17 @@ export function parseContainer(
             : value,
       };
     }, {});
+
+    if (isFullContainer) return { ...container, data };
+    return data;
   }
 
-  const fields = new Map<string, any>();
+  const data = new Map<string, any>();
 
-  for (const field of data) {
+  for (const field of rawData) {
     const val = field.value;
 
-    fields.set(
+    data.set(
       field.name,
       isObject(val)
         ? parseContainer([val])
@@ -56,7 +60,9 @@ export function parseContainer(
     );
   }
 
-  return fields;
+  if (isFullContainer) return { ...container, data };
+
+  return data;
 }
 
 type FieldType =
@@ -80,7 +86,7 @@ type GetField = {
   ): undefined;
 
   <T>(
-    content: Map<string, any>,
+    content: { data: Map<string, any> } | Map<string, any>,
     fieldName: string,
     opts?: GetFieldOpts,
   ): T extends any ? T : FieldType | undefined;
@@ -92,11 +98,11 @@ export const getField: GetField = (
   { returnObject } = { returnObject: false },
 ) => {
   if (!content) return undefined;
+  const data = (content as any)?.data || content;
 
-  const data =
-    content instanceof Map ? content.get(fieldName) : content[fieldName];
+  const result = data instanceof Map ? data.get(fieldName) : data[fieldName];
 
-  if (!(data instanceof Map)) return data;
+  if (!(result instanceof Map)) return result;
 
-  return returnObject ? Object.fromEntries(data) : data;
+  return returnObject ? Object.fromEntries(result) : result;
 };
